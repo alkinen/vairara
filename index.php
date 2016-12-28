576e6543ade515b34a3117b2e6702a4909782b1d
alkinen <olegkvd@gmail.com>
2016-12-28 04:28:17 +0200

    First steps. PHP.
    Learning

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+$name = "Nubas";
+$age = 25;
+
+function wellcome_Message($name, $age)
+{
+    echo "Wellcome to PHP " . $name . "</br>Your age is " . $age;
+}
+
+wellcome_Message($name, $age);
+	
+	

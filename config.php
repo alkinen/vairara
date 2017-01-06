473fa3380a1b8ab819230f0609add6afeeabf0b1
alkinen <olegkvd@gmail.com>
2017-01-06 07:25:31 +0200

    Starting the Work

--- config.php
+++ config.php
@@ -1,4 +1,16 @@
 <?php
+DEFINE(CORE_PATH, "/core/")
+//%host% - Host name
+//%aliases% - Aliases name
+//%sprogdir% - OpenServer directory
+//%ssitedir%/" - WebSite directory
+//%hostdir%" - Host directory
+//%mysqlport% - MySQL port
+//%ip% - ip address
+//%ips% - ip addreses
+//%httpport% - HTTP port
+//%httpsport% - HTTPS port
+//%httpbackport% - HTTP Back  port
 //Конфигурация подключения к Базе Данных
 $DB_HOST = "localhost";// Сервер БД
 $DB_NAME = "learning";// Имя БД
